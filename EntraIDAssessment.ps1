2bb744064693ad87b1f57a8d07b678b133e43711
b0k4nRb <stabelini.bruno@gmail.com>
2025-01-14 15:58:24 -0300

    Update EntraIDAssessment.ps1

--- EntraIDAssessment.ps1
+++ EntraIDAssessment.ps1
@@ -1,10 +1,34 @@
-# Conectar ao EntraID
-Connect-AzureAD
+# Verificar se o módulo Microsoft.Graph está instalado
+if (-not (Get-Module -ListAvailable -Name Microsoft.Graph)) {
+    Install-Module -Name Microsoft.Graph -Force -Scope CurrentUser
+}
 
-# Buscar grupos privilegiados
-$privilegedGroups = Get-AzureADDirectoryRole | Where-Object { $_.DisplayName -match "Admin" }
+# Importar o módulo Microsoft.Graph
+Import-Module Microsoft.Graph
 
-# Exibir os grupos privilegiados
-$privilegedGroups | ForEach-Object {
-    Write-Output "Group: $($_.DisplayName)"
+# Autenticar usando token
+#$clientId = "ENTER HERE"
+#$tenantId = "ENTER HERE"
+#$clientSecret = "ENTER HERE"
+
+$body = @{
+    client_id     = $clientId
+    scope         = "https://graph.microsoft.com/.default"
+    client_secret = $clientSecret
+    grant_type    = "client_credentials"
+}
+
+$response = Invoke-RestMethod -Method Post -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -ContentType "application/x-www-form-urlencoded" -Body $body
+$token = $response.access_token
+
+# Buscar grupos privilegiados usando Microsoft Graph API
+$headers = @{
+    Authorization = "Bearer $token"
+}
+
+$privilegedGroups = Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/directoryRoles" -Headers $headers
+
+# Filtrar e exibir os grupos privilegiados
+$privilegedGroups.value | Where-Object { $_.displayName -match "Admin" } | ForEach-Object {
+    Write-Output "Group: $($_.displayName)"
 }
